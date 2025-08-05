490339276bdfb4edc8e382fc3787b48f2bebaab8
Jonila-Murtezi <jonila1murtezi@gmail.com>
2025-08-05 16:56:50 +0200

    Validation

--- forms/src/App.tsx
+++ forms/src/App.tsx
@@ -7,33 +7,35 @@ function App() {
   const [email, setEmail] = useState("");
   const [date, setDate] = useState("");
 
+
+
   return (
     <div className="app">
       <form>
         <div>
           <label> Enter your firstname
-            <input type="text" placeholder="first name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+            <input type="text" placeholder="first name" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
           </label>
         </div>
 
         <div>
           <label> Enter your lastname
-            <input type="text" placeholder="last name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+            <input type="text" placeholder="last name" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
           </label>
         </div>
 
         <div>
           <label> Enter your email address
-            <input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
           </label>
         </div>
 
         <div>
           <label> Enter your date of birth
-            <input type="date" placeholder="date of birth" value={date} onChange={(e) => setDate(e.target.value)} />
+            <input type="date" placeholder="date of birth" value={date} onChange={(e) => setDate(e.target.value)} required />
           </label>
         </div>
-
+        <button type="submit"> Submit</button>
       </form>
     </div>
   )
