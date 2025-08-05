05b3b8c2ef170b82cc23a8b6491a885974c3b507
Jonila-Murtezi <jonila1murtezi@gmail.com>
2025-08-05 16:31:57 +0200

    Initialized the project

---
+++ forms/src/App.tsx
@@ -0,0 +1,42 @@
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [date, setDate] = useState("");
+
+  return (
+    <div className="app">
+      <form>
+        <div>
+          <label> Enter your firstname
+            <input type="text" placeholder="first name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+          </label>
+        </div>
+
+        <div>
+          <label> Enter your lastname
+            <input type="text" placeholder="last name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+          </label>
+        </div>
+
+        <div>
+          <label> Enter your email address
+            <input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </label>
+        </div>
+
+        <div>
+          <label> Enter your date of birth
+            <input type="date" placeholder="date of birth" value={date} onChange={(e) => setDate(e.target.value)} />
+          </label>
+        </div>
+
+      </form>
+    </div>
+  )
+}
+
+export default App
